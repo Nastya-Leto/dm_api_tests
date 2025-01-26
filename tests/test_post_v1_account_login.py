e2a98f6e32454a6b47d0f6fa47d7c7f02316dae1
Anastasiya Zakharova <111999983+Nastya-Leto@users.noreply.github.com>
2025-01-26 20:17:05 +0400

    Добавление проверок PyHamcrest

--- tests/test_post_v1_account_login.py
+++ tests/test_post_v1_account_login.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from hamcrest import assert_that, has_property, starts_with, all_of, instance_of, has_properties, equal_to
+
+
 class TestLoginUser:
     def test_successful_login(self, account_helper, prepare_user):
         login = prepare_user.login
@@ -5,8 +10,18 @@ class TestLoginUser:
         email = prepare_user.email
 
         account_helper.register_new_user(login, password, email)
-        response = account_helper.user_login(login, password)
-        assert response.headers['x-dm-auth-token'], f'Токен для пользователя не был получен'
+        response = account_helper.user_login(login, password, validate_response=True)
+        assert_that(response, all_of(
+            has_property('resource', has_property('login', starts_with('aanastya'))),
+            has_property('resource', has_property('registration', instance_of(datetime))),
+            has_property('resource', has_properties('rating', has_properties({
+                "enabled": equal_to(True),
+                "quality": equal_to(0),
+                "quantity": equal_to(0)
+            }))
+                         )))
+
+        print(response)
 
     def test_unsuccessful_login(self, account_helper, prepare_user):
         login = prepare_user.login
@@ -16,5 +31,5 @@ class TestLoginUser:
         response = account_helper.register_new_user(login, password, email, with_activate=False)
         assert response.status_code == 201, f'Пользователь не был создан{response.text}'
 
-        response = account_helper.user_login(login, password)
+        response = account_helper.user_login(login, password, validate_headers=False)
         assert response.status_code == 403, f'Пользователь был авторизован без активации{response.text}'
