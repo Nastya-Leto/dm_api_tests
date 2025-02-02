b00ef7c0682e985b81cb0c138e427e71e30da8ac
Anastasiya Zakharova <111999983+Nastya-Leto@users.noreply.github.com>
2025-02-02 09:10:37 +0400

    Добавление разметки аллюр

--- tests/test_put_v1_account_token.py
+++ tests/test_put_v1_account_token.py
@@ -1,3 +1,4 @@
+import allure
 import structlog
 
 from chekers.http_chekers import check_status_kode_http
@@ -6,9 +7,11 @@ structlog.configure(
     processors=[
         structlog.processors.JSONRenderer(indent=4, ensure_ascii=True, sort_keys=True)])
 
-
+@allure.suite('Тесты на проверку метода post_v1_account_token')
+@allure.sub_suite('Проверка активации пользователя')
 class TestActivationUser:
 
+    @allure.title('Проверка успешной активации пользователя')
     def test_successful_activation_user(self, account_helper, prepare_user):
         login = prepare_user.login
         password = prepare_user.password
@@ -17,6 +20,7 @@ class TestActivationUser:
             account_helper.register_new_user(login, password, email)
             account_helper.user_login(login, password)
 
+    @allure.title('Проверка неуспешной активации пользователя, при отсутствии токена')
     def test_unsuccessful_activation_user(self, account_helper, prepare_user):
         login = prepare_user.login
         password = prepare_user.password
