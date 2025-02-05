0e5429cb9f3ea1a5778f66f7d905f4f334f823e5
Anastasiya Zakharova <111999983+Nastya-Leto@users.noreply.github.com>
2025-02-05 20:20:56 +0400

    первый комммит в гитлаб

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ import structlog
 structlog.configure(
     processors=[
         structlog.processors.JSONRenderer(indent=4, ensure_ascii=True, sort_keys=True)])
+ #jkjk
 
 
 @pytest.fixture(scope="session")
